d052d8cc215f8339d89ed7f669b2318f75394daa
IsaacElenbaas <isaacelenbaas@gmail.com>
2023-01-28 12:14:57 -0500

    working without multiple encoding threads

--- RWAI.cs
+++ RWAI.cs
@@ -7,18 +7,21 @@ namespace RWAI {
 	[BepInEx.BepInPlugin("isaacelenbaas.rwai", "RWAI", "1.0.0")]
 
 	public class RWAI : BepInEx.BaseUnityPlugin {
-		// TODO
-		const int frameBacklog = 1;
+		const int frameBacklog = 20;
 
 		private bool record = true;
 		// just in case
 		private bool recordThis = false;
 
-		System.Collections.Concurrent.ConcurrentQueue<Unity.Collections.NativeArray<byte>> availableFrames = new System.Collections.Concurrent.ConcurrentQueue<Unity.Collections.NativeArray<byte>>();
-		System.Collections.Concurrent.ConcurrentQueue<UnityEngine.Rendering.AsyncGPUReadbackRequest> queuedFrames = new System.Collections.Concurrent.ConcurrentQueue<UnityEngine.Rendering.AsyncGPUReadbackRequest>();
+		System.Collections.Generic.Queue<Unity.Collections.NativeArray<byte>> availableFrames = new System.Collections.Generic.Queue<Unity.Collections.NativeArray<byte>>();
+		System.Collections.Generic.Queue<UnityEngine.RenderTexture> availableFrameBuffers = new System.Collections.Generic.Queue<UnityEngine.RenderTexture>();
+		System.Collections.Concurrent.ConcurrentQueue<Unity.Collections.NativeArray<byte>> queuedFrames = new System.Collections.Concurrent.ConcurrentQueue<Unity.Collections.NativeArray<byte>>();
+		System.Collections.Concurrent.ConcurrentQueue<UnityEngine.Rendering.AsyncGPUReadbackRequest> queuedFrameRequests = new System.Collections.Concurrent.ConcurrentQueue<UnityEngine.Rendering.AsyncGPUReadbackRequest>();
 		System.Threading.Semaphore availableFramesSem = new System.Threading.Semaphore(0, frameBacklog);
-		System.Threading.Semaphore    queuedFramesSem = new System.Threading.Semaphore(0, frameBacklog+1);
+		System.Threading.Semaphore    queuedFramesSem = new System.Threading.Semaphore(0, frameBacklog);
+		int queuedSemCount = 0;
 		uint skipQueuedSemRelease = 0;
+		System.Threading.Mutex frameMut = new System.Threading.Mutex();
 
 		public void OnEnable() { On.RainWorld.OnModsInit += OnModsInit; }
 
@@ -42,7 +45,7 @@ namespace RWAI {
 		private void ProcessManager_Update(On.ProcessManager.orig_Update orig, ProcessManager self, float deltaTime) {
 			// I would like to change this to 1 along with targetFrameRate, but it is set in a couple of places
 			// I don't care *that* much about one dropped/extra frame every once in a while
-			orig(self, 1f/40);
+			orig(self, 1f/20);
 		}
 
 		private System.Net.Sockets.NetworkStream ipc;
@@ -53,6 +56,7 @@ namespace RWAI {
 			if(lastWrite != null) await lastWrite;
 			// WriteAsync does not start unless awaited (which we don't want to do to let game loop still run)
 			lastWrite = System.Threading.Tasks.Task.Run(() => ipc.Write(data, 0, data.Length));
+			ipc.Write(data, 0, data.Length);
 		}
 
 		private void RoomCamera_DrawUpdate(On.RoomCamera.orig_DrawUpdate orig, RoomCamera self, float timeStacker, float timeSpeed) {
@@ -65,8 +69,6 @@ namespace RWAI {
 		UnityEngine.Experimental.Rendering.GraphicsFormat frameFormat;
 		int frameWidth;
 		int frameHeight;
-		// TODO: move, take from pool somehow? not sure how to know when one's done
-		UnityEngine.RenderTexture frameBuffer = new UnityEngine.RenderTexture(UnityEngine.Screen.currentResolution.width, UnityEngine.Screen.currentResolution.height, 0);
 		System.Collections.IEnumerator CaptureFrames() {
 			UnityEngine.RenderTexture tempFrameBuffer = new UnityEngine.RenderTexture(UnityEngine.Screen.currentResolution.width, UnityEngine.Screen.currentResolution.height, 0);
 			UnityEngine.Vector2 scale  = new UnityEngine.Vector2(1, -1);
@@ -74,53 +76,75 @@ namespace RWAI {
 			// TODO: Maybe Screen.currentResolution or something else instead?
 			for(int i = 0; i < frameBacklog; i++) {
 				availableFrames.Enqueue(new Unity.Collections.NativeArray<byte>(UnityEngine.Screen.currentResolution.width*UnityEngine.Screen.currentResolution.height*4, Unity.Collections.Allocator.Persistent, Unity.Collections.NativeArrayOptions.UninitializedMemory));
+				availableFrameBuffers.Enqueue(new UnityEngine.RenderTexture(UnityEngine.Screen.currentResolution.width, UnityEngine.Screen.currentResolution.height, 0));
 				availableFramesSem.Release();
 			}
 			while(true) {
 				yield return new UnityEngine.WaitForEndOfFrame();
 				if(!recordThis) continue;
 				recordThis = false;
-				if(queuedFrames.Count == frameBacklog) {
-					UnityEngine.Rendering.AsyncGPUReadbackRequest request;
-					queuedFrames.TryPeek(out request);
-					request.WaitForCompletion();
-					// the reason for this weirdness - where this normally happens won't if this thread is waiting on availableFramesSem
-					// would cause deadlock, WaitForCompletion might be enough to prevent it but not trusting and doing this
-					queuedFramesSem.Release();
-					skipQueuedSemRelease++;
+				// using mutex here because technically all frames could be taken for processing between "at limit" check and peek
+				// not using it to make skipQueuedSemRelease skip checks happen after it is incremented for this run or to check-and-set skipQueuedSemRelease because FrameAvailable is called by Unity in main thread at some start/end of frame (which is the problem this tackles in the first place)
+				// lil' performance boost, don't fight for mutex when unnecessary
+				// Releasing queuedFramesSem isn't enough to decrement from queuedFrames.count, need to not have FrameAvailables trigger then this, pushing queuedFramesSem over max, hence queuedSemCount
+				if(queuedFrameRequests.Count == frameBacklog && queuedSemCount != frameBacklog) {
+					frameMut.WaitOne();
+					if(queuedFrameRequests.Count == frameBacklog) {
+						UnityEngine.Rendering.AsyncGPUReadbackRequest request;
+						queuedFrameRequests.TryPeek(out request);
+						frameMut.ReleaseMutex();
+						request.WaitForCompletion();
+						// the reason for this weirdness - where this normally happens won't if this thread is waiting on availableFramesSem
+						queuedFramesSem.Release();
+						System.Threading.Interlocked.Increment(ref queuedSemCount);
+						skipQueuedSemRelease++;
+					}
+					else frameMut.ReleaseMutex();
 				}
 				UnityEngine.ScreenCapture.CaptureScreenshotIntoRenderTexture(tempFrameBuffer);
-				UnityEngine.Graphics.Blit(tempFrameBuffer, frameBuffer, scale, offset);
+				availableFramesSem.WaitOne();
+				Unity.Collections.NativeArray<byte> frame = availableFrames.Dequeue();
+				UnityEngine.RenderTexture frameBuffer = availableFrameBuffers.Dequeue();
+				availableFrames.Enqueue(frame);
+				availableFrameBuffers.Enqueue(frameBuffer);
 				if(initFrame) {
 					initFrame = false;
 					frameFormat = frameBuffer.graphicsFormat;
 					frameWidth = frameBuffer.width; frameHeight = frameBuffer.height;
 				}
-				availableFramesSem.WaitOne();
-				Unity.Collections.NativeArray<byte> frame; availableFrames.TryDequeue(out frame);
-				//UnityEngine.Rendering.CommandBuffer cb = new UnityEngine.Rendering.CommandBuffer();
-				//cb.RequestAsyncReadbackIntoNativeArray(ref frame, frameBuffer, 0, FrameAvailable);
-				//UnityEngine.Graphics.ExecuteCommandBuffer(cb);
-				queuedFrames.Enqueue(UnityEngine.Rendering.AsyncGPUReadback.RequestIntoNativeArray(ref frame, frameBuffer, 0, FrameAvailable));
+				UnityEngine.Graphics.Blit(tempFrameBuffer, frameBuffer, scale, offset);
+				queuedFrames.Enqueue(frame);
+				queuedFrameRequests.Enqueue(UnityEngine.Rendering.AsyncGPUReadback.RequestIntoNativeArray(ref frame, frameBuffer, 0, FrameAvailable));
 			}
 		}
 		private void FrameAvailable(UnityEngine.Rendering.AsyncGPUReadbackRequest request) {
-			if(skipQueuedSemRelease == 0)
+			if(skipQueuedSemRelease == 0) {
 				queuedFramesSem.Release();
+				System.Threading.Interlocked.Increment(ref queuedSemCount);
+			}
 			else skipQueuedSemRelease--;
 		}
 		private void ProcessFrames() {
 			while(true) {
 				queuedFramesSem.WaitOne();
+				System.Threading.Interlocked.Decrement(ref queuedSemCount);
 				UnityEngine.Rendering.AsyncGPUReadbackRequest request;
-				while(queuedFrames.TryPeek(out request) && request.done) {
-					queuedFrames.TryDequeue(out request);
-					Unity.Collections.NativeArray<byte> frame = request.GetData<byte>();
+				while(queuedFrameRequests.TryPeek(out request) && request.done) {
+					Unity.Collections.NativeArray<byte> frame;
+					frameMut.WaitOne();
+					queuedFrames.TryDequeue(out frame);
+					queuedFrameRequests.TryDequeue(out request);
+					frameMut.ReleaseMutex();
+					// hasError is toggled after one frame, see AsyncGPUReadbackRequest docs
+					// data should be Disposed of at that point but I have set as persistent
+					// try was necessary when using GetData (just dropped frames) but seems good with queuedFrames
+					//WriteIPC(request.hasError ? "BAD\n" : "GOOD\n");
+					//try {
 					Unity.Collections.NativeArray<byte> nativeData = UnityEngine.ImageConversion.EncodeNativeArrayToJPG(frame, frameFormat, (uint)frameWidth, (uint)frameHeight, quality:100);
 					byte[] data = nativeData.ToArray();
 					ipc.Write(data, 0, data.Length);
 					nativeData.Dispose();
-					availableFrames.Enqueue(frame);
+					//} catch {}
 					availableFramesSem.Release();
 				}
 			}
